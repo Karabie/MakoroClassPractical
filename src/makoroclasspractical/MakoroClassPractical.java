fc45aefe02a4a8ecb7704e9167cfbaa829948476
@KaraboMakoro <k.m.makoro@gmail.com>
2024-04-08 12:52:33 +0000

    Initial commit

---
+++ src/makoroclasspractical/MakoroClassPractical.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package makoroclasspractical;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class MakoroClassPractical {
+
+    public static void main(String[] args) {
+        
+        double a = Double.parseDouble(JOptionPane.showInputDialog("Enter Number one"));
+        double b = Double.parseDouble(JOptionPane.showInputDialog("Enter Number two"));     
+        JOptionPane.showMessageDialog(null,"Sum is: " + (a+b));
+        JOptionPane.showMessageDialog(null,"Average: " + ((a + b)/2));
+        
+    }
+    
+}
